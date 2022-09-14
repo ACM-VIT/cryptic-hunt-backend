53604c3a374dc873510093a08b4a32a7420bd74b
Aarav Arora <aaravarora.works@gmail.com>
2022-09-14 22:15:15 +0530

    added profile get feature

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -4,7 +4,7 @@ import { Prisma, PrismaClient } from "@prisma/client";
 import { u_req } from "../models/req";
 const prisma = new PrismaClient();
 const user_router = express.Router();
-user_router.post("/signup", async (req: u_req, res: Response) => {
+user_router.post("/profile", async (req: u_req, res: Response) => {
   const { gender, rollnumber, phonenumber } = req.body;
   try {
     const user_signup = await prisma.user.update({
@@ -21,9 +21,24 @@ user_router.post("/signup", async (req: u_req, res: Response) => {
   } catch (e) {
     if (e instanceof Prisma.PrismaClientKnownRequestError) {
       console.log(e);
-      throw "an error occured while signing up";
+      res.status(501).json({"error" : "unabele to modify profile details"});
     }
   }
 });
 
+user_router.get("/profile", async (req: u_req, res: Response)=>{
+  try{
+  const userProfile = await prisma.user.findUnique({
+    where : {
+      id : req.user.id
+    }
+  })
+  res.json(userProfile)
+}
+catch (e){
+   if (e instanceof Prisma.PrismaClientKnownRequestError) {
+     console.log(e);
+     res.status(501).json({"error" : "an error occured while getting user profile"}) ;
+   }
+}})
 export default user_router;
