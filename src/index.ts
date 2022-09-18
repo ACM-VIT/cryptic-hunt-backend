1218773aa7778ec1f80a3c71ac4171dd218f3d28
Swarup Kharul <swarupkharulsk@gmail.com>
2022-09-18 17:44:32 +0530

    feat. add sentry

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
-import swaggerUi from "swagger-ui-express";
 import { authMiddleware } from "./auth";
 import teamsRouter from "./routes/TeamRouter";
 import adminRouter from "./routes/AdminRouter";
 import rateLimit from "express-rate-limit";
 import helmet from "helmet";
 import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import cors from "cors";
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 import usersRouter from "./routes/UserRouter";
@@ -16,10 +16,26 @@ dotenv.config();
 
 const app = express();
 
-Sentry.init({ dsn: process.env.SENTRY_DSN });
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
 
-// The request handler must be the first middleware on the app
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
 app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(helmet());
 
@@ -41,15 +57,6 @@ app.use(
     origin: true,
     optionsSuccessStatus: 200,
     credentials: true,
-  })
-);
-app.use(
-  "/docs",
-  swaggerUi.serve,
-  swaggerUi.setup(undefined, {
-    swaggerOptions: {
-      url: "/swagger.json",
-    },
   })
 );
 
@@ -84,8 +91,21 @@ app.use("/users", usersRouter);
 app.use("/submissions", submissionsRouter);
 app.use("/questiongroups", questionGroupsRouter);
 
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
+
 // The error handler must be before any other error middleware and after all controllers
-app.use(Sentry.Handlers.errorHandler());
+app.use(
+  Sentry.Handlers.errorHandler({
+    shouldHandleError(error) {
+      if (error.status && error.status >= 400) {
+        return true;
+      }
+      return false;
+    },
+  })
+);
 
 const port = process.env.PORT || 8081;
 app.listen(port, () =>
