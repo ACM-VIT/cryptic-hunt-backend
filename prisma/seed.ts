743a07fb3ac9aa27221ef2cee693ab6daca612fd
Sankalp Mukim <sankalpmukim@gmail.com>
2022-09-28 18:42:49 +0530

    improve db seed command

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,39 +1,43 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient, PrismaPromise } from "@prisma/client";
 import { readCsv } from "../src/controllers/verify.controllers";
 
 const prisma = new PrismaClient();
 
 async function main() {
+  const transactions: PrismaPromise<Prisma.BatchPayload>[] = [
+    prisma.timeLineEvents.deleteMany(),
+    prisma.liveConfig.deleteMany(),
+    prisma.whitelist.deleteMany(),
+    prisma.submission.deleteMany(),
+    prisma.team.deleteMany(),
+    prisma.user.deleteMany(),
+  ];
+
   const data = await readCsv();
 
   //   whitelist createMany, data.map
-  const whitelist = await prisma.whitelist.createMany({
-    data: data.map((record) => {
-      return {
-        email: record.email,
-      };
-    }),
-    skipDuplicates: true,
-  });
+  transactions.push(
+    prisma.whitelist.createMany({
+      data: data.map((record) => {
+        return {
+          email: record.email,
+        };
+      }),
+      skipDuplicates: true,
+    })
+  );
 
-  //   create live config config if not exists
-  let liveConfig = await prisma.liveConfig.findFirst({
-    where: {
-      id: "1",
-    },
-  });
-  if (!liveConfig) {
-    liveConfig = await prisma.liveConfig.create({
+  return await Promise.all([
+    await prisma.$transaction(transactions),
+    await prisma.liveConfig.create({
       data: {
-        id: "1",
+        mainText: "Event starts in ...",
+        phaseText: "Are you excited?",
+        time: new Date("2022-09-30T10:00:00.000Z"),
+        currentPhase: -1,
       },
-    });
-  }
-
-  return {
-    whitelist,
-    liveConfig,
-  };
+    }),
+  ]);
 }
 
 main()
