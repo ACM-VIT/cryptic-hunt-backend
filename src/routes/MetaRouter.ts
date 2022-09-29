ac1c8dc4512947ccd337eef109458ebc7a4fe218
Sankalp Mukim <sankalpmukim@gmail.com>
2022-09-29 16:52:39 +0530

    feat: meta updates page

--- src/routes/MetaRouter.ts
+++ src/routes/MetaRouter.ts
@@ -1,7 +1,9 @@
 import { Request, Response, Router } from "express";
 import { prisma } from "..";
+import { adminMiddleware } from "../middleware/admin.middleware";
 const router = Router();
 
+// GET timeline from timelineEvents table
 router.get("/timeline", async (req: Request, res: Response) => {
   return res.json(
     await prisma.timeLineEvents.findMany({
@@ -11,5 +13,64 @@ router.get("/timeline", async (req: Request, res: Response) => {
     })
   );
 });
+
+// GET updates from updates table
+router.get("/updates", async (req: Request, res: Response) => {
+  const data = await prisma.updates.findMany({
+    orderBy: {
+      id: "asc",
+    },
+  });
+  return res.json(
+    data.map((item) => {
+      return {
+        id: item.id,
+        title: item.title,
+        description: item.content,
+        date: item.createdAt,
+      };
+    })
+  );
+});
+
+// POST create new update
+router.post(
+  "/updates",
+  adminMiddleware,
+  async (req: Request, res: Response) => {
+    const { title, description } = req.body;
+    if (!title || !description) {
+      return res.status(400).json({
+        message: "Missing required fields",
+      });
+    }
+
+    if (typeof title !== "string" || typeof description !== "string") {
+      return res.status(400).json({
+        message: "Invalid type of title or description",
+      });
+    }
+    try {
+      return res.json(
+        await prisma.updates.create({
+          data: {
+            title,
+            content: description,
+          },
+        })
+      );
+    } catch (error) {
+      if (error instanceof Error) {
+        return res.status(400).json({
+          message: error.message,
+        });
+      } else {
+        return res.status(500).json({
+          message: "An error occured :(",
+        });
+      }
+    }
+  }
+);
 
 export default router;
